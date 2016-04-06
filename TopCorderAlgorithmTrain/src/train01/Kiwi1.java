232c1611ff249855c665932715164d00cdb52164
gopae02 <gopae02@gmail.com>
2016-04-06 23:31:27 +0900

    -16/04/06 추가  기존 해법 + 개선1 + 개선2

--- TopCorderAlgorithmTrain/src/train01/Kiwi1.java
+++ TopCorderAlgorithmTrain/src/train01/Kiwi1.java
@@ -18,7 +18,7 @@ package train01;
  * 
  * */
 
-public class Kiwi {
+public class Kiwi1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -35,49 +35,14 @@ public class Kiwi {
 	
 	public static int[] thePouring(int[] capas, int[] bottles, int[] fromId, int[] toId){
 		for(int i=0; i< fromId.length; i++){	// 5 repeat
-			int from = fromId[i];	
-				// 0=> from 2	
-				// 1=> from 3	
-				// 2=> from 2
-				// 3=> from 0
-				// 4=> from 1
-			int to = toId[i];		
-				// 0=> to 0		
-				// 1=> to 1		
-				// 2=> to 1
-				// 3=> to 3
-				// 4=> to 2
-			int sum = bottles[from] + bottles[to];	
-			// bottle[2] + bottle[0] => 956956 (sum)
-			// bottle[3] + bottle[1] => 956956 (sum)
-			// bottle[2] + bottle[1] => 256956 + 800000 = 1056956 (sum)
-			// bottle[0] + bottle[3] => 700000 + 156956 = 856956  (sum)
-			// bottle[1] + bottle[2] => 800000 + 256956 = 1056956 (sum)
-				
-			int nam = sum - capas[to];				
-			// 956956 - 700000 		(capa[0]) => 256956 (nam)
-			// 956956 - 800000 		(capa[1]) => 156956 (nam)
-			// 1056956 - 800000 	(capa[1]) => 256956 (nam)
-			// 856956 -  1000000 	(capa[3]) => -143044 (nam)
-			// 1056956 - 900000 	(capa[2]) => 156956 (nam)
 			
-			if( nam > 0) {
-				bottles[from] = nam;					
-					// bottle[2] = 256956
-					// bottle[3] = 156956
-					// bottle[2] = 256956
-					// bottle[1] = 156956
-				bottles[to] = capas[to];				
-					// bottle[0] = 700000
-					// bottle[1] = 800000
-					// bottle[1] = 800000
-					// bottle[2] = 900000
-			} else {
-				bottles[from] = 0;
-					// bottle[0] = 0;
-				bottles[to] = sum;
-					// bottle[3] = 856956;
-			}			
+			int f = fromId[i];
+			int t = toId[i];
+			
+			// if 조건문이 아닌 최소값 함수 사용 java.lang.Math
+			int vol = Math.min(bottles[f], capas[t] - bottles[t]);
+			bottles[f] -= vol;
+			bottles[t] += vol;
 		}		
 		return bottles;
 	}
--- TopCorderAlgorithmTrain/src/train01/Kiwi2.java
+++ TopCorderAlgorithmTrain/src/train01/Kiwi2.java
@@ -18,7 +18,7 @@ package train01;
  * 
  * */
 
-public class Kiwi {
+public class Kiwi2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -35,49 +35,14 @@ public class Kiwi {
 	
 	public static int[] thePouring(int[] capas, int[] bottles, int[] fromId, int[] toId){
 		for(int i=0; i< fromId.length; i++){	// 5 repeat
-			int from = fromId[i];	
-				// 0=> from 2	
-				// 1=> from 3	
-				// 2=> from 2
-				// 3=> from 0
-				// 4=> from 1
-			int to = toId[i];		
-				// 0=> to 0		
-				// 1=> to 1		
-				// 2=> to 1
-				// 3=> to 3
-				// 4=> to 2
-			int sum = bottles[from] + bottles[to];	
-			// bottle[2] + bottle[0] => 956956 (sum)
-			// bottle[3] + bottle[1] => 956956 (sum)
-			// bottle[2] + bottle[1] => 256956 + 800000 = 1056956 (sum)
-			// bottle[0] + bottle[3] => 700000 + 156956 = 856956  (sum)
-			// bottle[1] + bottle[2] => 800000 + 256956 = 1056956 (sum)
-				
-			int nam = sum - capas[to];				
-			// 956956 - 700000 		(capa[0]) => 256956 (nam)
-			// 956956 - 800000 		(capa[1]) => 156956 (nam)
-			// 1056956 - 800000 	(capa[1]) => 256956 (nam)
-			// 856956 -  1000000 	(capa[3]) => -143044 (nam)
-			// 1056956 - 900000 	(capa[2]) => 156956 (nam)
-			
-			if( nam > 0) {
-				bottles[from] = nam;					
-					// bottle[2] = 256956
-					// bottle[3] = 156956
-					// bottle[2] = 256956
-					// bottle[1] = 156956
-				bottles[to] = capas[to];				
-					// bottle[0] = 700000
-					// bottle[1] = 800000
-					// bottle[1] = 800000
-					// bottle[2] = 900000
-			} else {
-				bottles[from] = 0;
-					// bottle[0] = 0;
-				bottles[to] = sum;
-					// bottle[3] = 856956;
-			}			
+			// 처음 이동량을 생각햇을 때 조건 분기에서 실수 발생 가능
+			// 이동량 무시 -> 옮길 주스와 기존 주스 양의 총합이 일정하다는 것과 옮길주스는 주스 총량과
+			// 기존 주스병의 용량중에 작은 값이 된다는 것을 이용
+			// 기존 주스 : "옮길 주스와 기존 주스의 총합" 과 "기존 주스 병의 용량"에서 작은 값.
+			// 옮길 주스 : "옮길 주스와 기준 주스의 총합"에서 위의 값을 제외한 값
+			int sum = bottles[fromId[i]] + bottles[toId[i]];
+			bottles[toId[i]] = Math.min(sum, capas[toId[i]]);
+			bottles[fromId[i]] = sum - bottles[toId[i]];
 		}		
 		return bottles;
 	}
